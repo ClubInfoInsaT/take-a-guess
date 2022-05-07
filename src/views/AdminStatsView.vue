303c1300e67f6add153d78a35dd18260663bfaaa
Gérald LEBAN <gerald.leban0@gmail.com>
2022-05-07 15:31:18 +0200

    Ajout d'un bouton pour montrer aux joueurs les résultat de la question

--- src/views/AdminStatsView.vue
+++ src/views/AdminStatsView.vue
@@ -18,7 +18,7 @@
       </div>
     </div>
     <div
-      class="flex flex-col justify-center mx-auto w-2/3 gap-2"
+      class="grid grid-cols-2 justify-center mx-auto w-2/3 gap-4 py-4 h-full"
       :class="
         playersStillAlive() + deadPlayers() === totalPlayers
           ? 'block'
@@ -28,23 +28,30 @@
       <button
         class="px-4 py-2 mb-2 bg-[#E40495] border-[#5E17EB] border-2 rounded-3xl fancy-shadow"
         :class="deadPlayers() !== totalPlayers ? 'block' : 'hidden'"
+        @click="showResultsToPlayers()"
+      >
+        <span class="uppercase text-xl text-white">Montrer résultats</span>
+      </button>
+      <button
+        class="px-4 py-2 mb-2 bg-[#E40495] border-[#5E17EB] border-2 rounded-3xl fancy-shadow"
+        :class="deadPlayers() !== totalPlayers ? 'block' : 'hidden'"
         @click="goNextQuestion()"
       >
-        <span class="uppercase text-3xl text-white">Continuer</span>
+        <span class="uppercase text-xl text-white">Continuer</span>
       </button>
 
       <button
         class="px-4 py-2 mb-2 bg-[#E40495] border-[#5E17EB] border-2 rounded-3xl fancy-shadow"
         @click="invalidate()"
       >
-        <span class="uppercase text-3xl text-white">Invalider</span>
+        <span class="uppercase text-xl text-white">Invalider</span>
       </button>
 
       <button
         class="px-4 py-2 mb-2 bg-[#E40495] border-[#5E17EB] border-2 rounded-3xl fancy-shadow"
         @click="endGame()"
       >
-        <span class="uppercase text-3xl text-white">Fin Partie</span>
+        <span class="uppercase text-xl text-white">Fin Partie</span>
       </button>
     </div>
   </div>
@@ -148,6 +155,10 @@ export default class AdminStatsView extends Vue {
    */
   deadPlayers() {
     return this.dead[0].data.reduce((a, b) => a + b, 0);
+  }
+
+  showResultsToPlayers() {
+    this.$socket.emit("question-stats");
   }
 
   /**
